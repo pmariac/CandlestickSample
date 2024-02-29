1d5c900cde9d238efa0ad8adbc0eeea5ae4d0fd5
Louis CAD <louis.cognault@gmail.com>
2024-02-29 16:48:30 +0100

    Remove no longer needed code in DualCharts
    SizeSynchronizer has been fixed to handle resizing

--- composeApp/src/commonMain/kotlin/DualCharts.kt
+++ composeApp/src/commonMain/kotlin/DualCharts.kt
@@ -4,19 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import io.data2viz.charts.chart.Chart
 import io.data2viz.charts.chart.mark.domainSpecific.PriceMovement
 import io.data2viz.charts.layout.sizeManager
-import io.data2viz.geom.Size
-import io.data2viz.viz.VizContainer
 
 /**
  * Dual chart view, on the top the candlestick chart, on the bottom the volume histogram.
@@ -28,24 +24,16 @@ import io.data2viz.viz.VizContainer
  * Compose + Synchronizer, we are working on it.
  */
 @Composable
-fun DualCharts(
-	modifier: Modifier = Modifier
-) {
+fun DualCharts(modifier: Modifier = Modifier) {
 
 	// Storing the 2 charts
 	val priceChartState: MutableState<Chart<PriceMovement>?> = remember { mutableStateOf(null) }
 	val volumeChartState: MutableState<Chart<PriceMovement>?> = remember { mutableStateOf(null) }
 
-	// Storing the charts' sizes
-	var priceChartSize: Size by remember { mutableStateOf(Size(.0, .0)) }
-	var volumeChartSize: Size by remember { mutableStateOf(Size(.0, .0)) }
-
 	// This may be removed when the "resizing issue" will be resolved
 	SynchronizeEffect(
 		firstChartState = priceChartState,
-		secondChartState = volumeChartState,
-		firstChartSize = priceChartSize,
-		secondChartSize = volumeChartSize
+		secondChartState = volumeChartState
 	)
 
 	val dataset = remember { generateDataset(200) }
@@ -53,26 +41,18 @@ fun DualCharts(
 	// Default layout : 80% top is the candlestick, 20% bottom is the volume chart.
 	Column(modifier) {
 		Viz(modifier = Modifier.fillMaxWidth().weight(.8f)) {
-			it.keepSizeForSynchronization { newSize -> priceChartSize = newSize }
 			priceChartState.value = it.candleStick(dataset, 40)
 		}
 		Viz(modifier = Modifier.fillMaxWidth().weight(.2f)) {
-			it.keepSizeForSynchronization { newSize -> volumeChartSize = newSize }
 			volumeChartState.value = it.volumeHistogram(dataset, 40)
 		}
 	}
 }
 
-private fun VizContainer.keepSizeForSynchronization(newSize: (Size) -> Unit) {
-	newViz { onResize { w, h -> newSize(Size(w, h)) } }
-}
-
 @Composable
 private fun SynchronizeEffect(
 	firstChartState: State<Chart<PriceMovement>?>,
-	secondChartState: State<Chart<PriceMovement>?>,
-	firstChartSize: Size,
-	secondChartSize: Size
+	secondChartState: State<Chart<PriceMovement>?>
 ) {
 	val sizeManager = remember { sizeManager() }
 	val verticalSync = remember { sizeManager.vSynchro() }
@@ -94,11 +74,8 @@ private fun SynchronizeEffect(
 			secondChart.setCursorFor(event.selectedData.firstOrNull())
 		}
 		onDispose {
-			// should remove listeners
+			// We are not changing the charts.
+			// If we did, we would want to unregister all the listeners here.
 		}
-	}
-
-	LaunchedEffect(firstChartSize, secondChartSize) {
-		sizeManager.synchronize()
 	}
 }
