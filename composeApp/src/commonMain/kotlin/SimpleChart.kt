eb2ca0b675580bc331ab3c5e41a3e3f704446048
pierre <pierre@data2viz.io>
2024-02-29 18:30:31 +0100

    Optimize imports

--- composeApp/src/commonMain/kotlin/SimpleChart.kt
+++ composeApp/src/commonMain/kotlin/SimpleChart.kt
@@ -2,20 +2,8 @@ package io.data2viz.sample
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import io.data2viz.charts.chart.Chart
-import io.data2viz.charts.chart.mark.domainSpecific.PriceMovement
-import io.data2viz.charts.layout.sizeManager
-import io.data2viz.geom.Size
-import io.data2viz.viz.VizContainer
 
 /**
  * Just a simple candlestick chart, no volume chart, no size & events synchronization.
